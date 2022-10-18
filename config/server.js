508d36cd4ac5068c404edaf62db975a8634d1032
kronk0s <bessaadkhalil@gmail.com>
2022-10-18 08:16:11 +0100

    server

--- config/server.js
+++ config/server.js
@@ -1,7 +1,12 @@
 module.exports = ({ env }) => ({
-  host: env('HOST', '0.0.0.0'),
-  port: env.int('PORT', 1337),
+  host: env("HOST", "127.0.0.1"),
+  port: env.int("PORT", 1337),
+  url: "https://zippy-stroopwafel-eb73fa.netlify.app/",
+  admin: {
+    url: "https://zippy-stroopwafel-eb73fa.netlify.app/",
+    serveAdminPanel: false,
+  },
   app: {
-    keys: env.array('APP_KEYS'),
+    keys: env.array("APP_KEYS"),
   },
 });
